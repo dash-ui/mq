affa2280545b0cfd17754f1317f7eb36bd552e07
Jared Lunde <jared@getadmiral.com>
2020-05-02 21:39:20 -0600

    Fixes babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,6 @@
 module.exports = (api) => {
   const module = api.env('module')
   const esm = api.env('esm')
-  const umd = api.env('umd')
 
   return {
     presets: [
@@ -9,15 +8,10 @@ module.exports = (api) => {
         '@lunde/es',
         {
           env: {
-            modules: esm || module || umd ? false : 'commonjs',
+            modules: esm || module ? false : 'commonjs',
             targets: module
               ? {
                   browsers: '> 2%',
-                }
-              : umd
-              ? {
-                  browsers:
-                    '> 0.5%, ie >= 10, safari >= 9, firefox >= 43, ios >= 8',
                 }
               : {
                   node: esm ? '12' : '10',
