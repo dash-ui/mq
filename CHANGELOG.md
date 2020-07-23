23abe10efb1f8b951e2ca05768dd22f05b445e66
Jared Lunde <jared.lunde@gmail.com>
2020-07-22 19:27:47 -0600

    chore(release): 0.3.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,4 +2,16 @@
 
 All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
 
+## [0.3.0](https://github.com/dash-ui/mq/compare/v0.2.0...v0.3.0) (2020-07-23)
+
+### ⚠ BREAKING CHANGES
+
+- The `Variables` generic now comes before the `QueryNames` generic type
+
+### Bug Fixes
+
+- remove unused argument type def ([050b981](https://github.com/dash-ui/mq/commit/050b981dda63a1cd09d7c9b8e853a2efc097b776))
+
+* change generic type order ([9a75b99](https://github.com/dash-ui/mq/commit/9a75b994b7f312e72dc62737aa47bac55086a734))
+
 ## 0.2.0 (2020-07-04)
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,28 +1,25 @@
 import type {StyleObject, StyleCallback, DashVariables} from '@dash-ui/styles'
+/**
+ * A factory function that creates a utility for adding breakpoints and
+ * media queries to Dash styles
+ *
+ * @param mediaQueries A map of media query name/query pairs
+ */
 declare function mq<
-  QueryNames extends string,
-  Variables extends DashVariables = DashVariables
+  Variables extends DashVariables = DashVariables,
+  QueryNames extends string = string
 >(
   mediaQueries: MediaQueries<QueryNames>
 ): MediaQueryCssCallback<QueryNames, Variables>
 declare function mq<
-  QueryNames extends string,
-  Variables extends DashVariables = DashVariables
+  Variables extends DashVariables = DashVariables,
+  QueryNames extends string = string
 >(
   mediaQueries: MediaQueries<QueryNames>
 ): MediaQueryNameCallback<QueryNames, Variables>
 export default mq
 export declare type MediaQueries<QueryNames extends string> = {
   readonly [K in QueryNames]: string
-}
-export declare type MediaQueryObject<
-  QueryNames extends string,
-  Variables extends DashVariables = DashVariables
-> = {
-  readonly [K in QueryNames | 'default']?:
-    | string
-    | StyleObject
-    | StyleCallback<Variables>
 }
 declare type MediaQueryNameCallback<
   QueryNames extends string,
@@ -32,12 +29,14 @@ declare type MediaQueryCssCallback<
   QueryNames extends string,
   Variables extends DashVariables = DashVariables
 > = (
-  queryName: QueryNames | MediaQueryObject<QueryNames, Variables>,
-  css?: string | StyleObject | StyleCallback<Variables>
+  queryName: QueryNames | MediaQueryObject<QueryNames, Variables>
 ) => (variables: Variables) => string
-export declare type MediaQueryCallback<
+export declare type MediaQueryObject<
   QueryNames extends string,
   Variables extends DashVariables = DashVariables
-> =
-  | MediaQueryNameCallback<QueryNames, Variables>
-  | MediaQueryCssCallback<QueryNames, Variables>
+> = {
+  readonly [K in QueryNames | 'default']?:
+    | string
+    | StyleObject
+    | StyleCallback<Variables>
+}
